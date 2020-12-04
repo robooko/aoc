8cad5a2e65aa05ff568528bb95609cfa3429d2b9
robert okoroafor <robert@icentric.co.uk>
2020-12-04 21:11:16 +0000

    wip

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,27 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Text.RegularExpressions;
 
-namespace ConsoleApp1
+namespace Day2
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var stopwatch = new Stopwatch();
             string[] allLines = File.ReadAllLines("textfile1.txt");
             long fitRange = 0;
             long matchOnePosition = 0;
+            stopwatch.Start();
             foreach (string line in allLines)
             {
                 var split = line.Split(':');
-                var rule = new PasswordRule(split[0].Trim());
-                var password = split[1].Trim();
-
+                var rule = new PasswordRule(split[0]);
+                var password = split[1];
                 fitRange = rule.FitsRange(password) ? fitRange + 1 : fitRange;
                 matchOnePosition = rule.MatchesOnePosition(password) ? matchOnePosition + 1 : matchOnePosition;
             }
-
+            stopwatch.Stop();
+            Console.WriteLine($"Ran in {stopwatch.ElapsedMilliseconds}.");
             Console.WriteLine($"{fitRange} passwords fit the range");
             Console.WriteLine($"{matchOnePosition} passwords match one position");
         }
@@ -30,29 +33,28 @@ namespace ConsoleApp1
 
     class PasswordRule
     {
-        public string match;
-        public int min;
-        public int max;
+        string match;
+        int min;
+        int max;
 
         public PasswordRule(string rule)
         {
             var ruleSplit = rule.Split(' ');
-            this.match = ruleSplit[1].Trim();
-            var minMaxSplit = ruleSplit[0].Trim().Split('-');
-            this.min = int.Parse(minMaxSplit[0]);
-            this.max = int.Parse(minMaxSplit[1]);
+            match = ruleSplit[1];
+            var minMaxSplit = ruleSplit[0].Split('-');
+            min = Convert.ToInt16(minMaxSplit[0]);
+            max = Convert.ToInt16(minMaxSplit[1]);
         }
 
         internal bool FitsRange(string password)
         {
-            var count = Regex.Matches(password, this.match).Count;
-            return (count >= min && count <= max);
+            var count = Regex.Matches(password, match).Count;
+            return count >= min && count <= max;
         }
 
         internal bool MatchesOnePosition(string password)
         {
-            var count = Regex.Matches(password, this.match).Count;
-            return ((password.ToCharArray()[min - 1].ToString() == match) ^ (password.ToCharArray()[max - 1].ToString() == match));
+            return password.Substring(min, 1).Equals(match) ^ password.Substring(max, 1).Equals(match);
         }
     }
 }
---
+++ Day1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] allLines = File.ReadAllLines("textfile1.txt");
+            for (int i = 0; i < allLines.Length; i++)
+            {
+                var options = allLines.Where((source, index) => index != i).ToArray();
+                var sum = get2020Position(options, int.Parse(allLines[i]));
+                if (sum > 0)
+                {
+                    Console.WriteLine($"{sum} is the answere");
+                }
+            }
+        }
+
+        private static int get2020Position(string[] options, int v)
+        {
+            for (int i = 0; i < options.Length; i++)
+            {
+                var a = int.Parse(options[i]);
+                if (v + a == 2020)
+                {
+                    Console.WriteLine($"{a} and {v}");
+                    return v * a;
+                }
+            }
+            return 0;
+        }
+    }
+}
