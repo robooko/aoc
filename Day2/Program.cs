0fb2547137208b572489318b3fcb9f4d37bdf380
robert okoroafor <robert@icentric.co.uk>
2020-12-05 08:18:29 +0000

    linq

--- Day2/Program.cs
+++ Day2/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace Day2
@@ -14,7 +15,9 @@ namespace Day2
             string[] allLines = File.ReadAllLines("textfile1.txt");
             long fitRange = 0;
             long matchOnePosition = 0;
+
             stopwatch.Start();
+
             foreach (string line in allLines)
             {
                 var split = line.Split(':');
@@ -23,7 +26,9 @@ namespace Day2
                 fitRange = rule.FitsRange(password) ? fitRange + 1 : fitRange;
                 matchOnePosition = rule.MatchesOnePosition(password) ? matchOnePosition + 1 : matchOnePosition;
             }
+
             stopwatch.Stop();
+
             Console.WriteLine($"Ran in {stopwatch.ElapsedMilliseconds}.");
             Console.WriteLine($"{fitRange} passwords fit the range");
             Console.WriteLine($"{matchOnePosition} passwords match one position");
@@ -33,28 +38,30 @@ namespace Day2
 
     class PasswordRule
     {
-        string match;
-        int min;
-        int max;
+        char _match;
+        int _min;
+        int _max;
 
         public PasswordRule(string rule)
         {
             var ruleSplit = rule.Split(' ');
-            match = ruleSplit[1];
             var minMaxSplit = ruleSplit[0].Split('-');
-            min = Convert.ToInt16(minMaxSplit[0]);
-            max = Convert.ToInt16(minMaxSplit[1]);
+
+            _match = Convert.ToChar(ruleSplit[1]);
+
+            _min = Convert.ToInt16(minMaxSplit[0]);
+            _max = Convert.ToInt16(minMaxSplit[1]);
         }
 
         internal bool FitsRange(string password)
         {
-            var count = Regex.Matches(password, match).Count;
-            return count >= min && count <= max;
+            var count = password.Count(x => x.Equals(_match));
+            return count >= _min && count <= _max;
         }
 
         internal bool MatchesOnePosition(string password)
         {
-            return password.Substring(min, 1).Equals(match) ^ password.Substring(max, 1).Equals(match);
+            return password[_min].Equals(_match) ^ password[_max].Equals(_match);
         }
     }
 }
