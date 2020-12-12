3423ea7bb88701144b0713852933c1b7f935385b
robert okoroafor <robert@icentric.co.uk>
2020-12-12 11:20:34 +0000

    Day7

--- Day7/Program.cs
+++ Day7/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
 
 namespace Day7
 {
@@ -6,7 +10,85 @@ namespace Day7
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] allLines = File.ReadAllLines("textfile1.txt");
+            var bags = new Bags(allLines);
+            Console.WriteLine($"Q1 answer is {bags.FindBags("shiny gold bag").Length}");
         }
+    }
+
+    class Bags
+    {
+        Collection<Bag> _allBags { get; set; }
+
+        public Bags(string[] data)
+        {
+            _allBags = new Collection<Bag>();
+
+            foreach(string strBags in data)
+            {
+                _allBags.Add(new Bag(strBags));
+            }
+
+            foreach (var bag in _allBags)
+            {
+                bag.SetBags(_allBags);
+            }
+        }
+
+        public Bag[] FindBags(string bagName)
+        {
+            var foundBags = new List<Bag>();
+
+            foreach (var bag in _allBags)
+            {
+                var hasBag = bag.HasBag(bagName);
+                if (hasBag)
+                {
+                    foundBags.Add(bag);
+                }
+            }
+
+            return foundBags.ToArray();
+        }
+    }
+
+
+    class Bag
+    {
+        string _bagName { get; init; }
+        string[] _bags { get; init; }
+        Bag[] _bagArray;
+
+        public Bag(string data)
+        {
+            var splits = data.Split(" contain");
+            _bagName = splits[0].TrimEnd('s');
+            _bags = splits[1].Split(',').Select(x => {
+                var ret = x.Trim();
+                ret = ret.TrimEnd('.');
+                return ret.Substring(ret.IndexOf(" ")); 
+            }).ToArray();
+        }
+
+        public void SetBags(Collection<Bag> AllBags)
+        {
+            var joinedBags = string.Join("", _bags);
+            _bagArray = AllBags.Where(x => joinedBags.Contains(x._bagName)).ToArray();
+        }
+
+        internal bool HasBag(string v)
+        {
+            foreach(var bag in this._bagArray)
+            {
+                if (bag._bagName.Contains(v))
+                    return true;
+                else if (bag.HasBag(v))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
     }
 }
